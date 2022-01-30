7997c1c5f92442c2949a8f6c31a6ad1847bbf7c9
julietvt <freshitpt@gmail.com>
2022-01-30 12:22:59 +0200

    config was created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-const db = mongoose.connection;
-
-mongoose
-  .connect('mongodb://127.0.0.1:27017/testpd2021')
-  .then((data) => console.log('Connection success'))
-  .catch((err) => console.log('Connection error', err));
+const app = require('./app');
+const http = require('http');
+const PORT = 27017;
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server is working on ${PORT}`);
+});
---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const env = process.env.NODE_ENV ?? 'development';
+const config = require('./../config/db')[env];
+
+mongoose
+  .connect(`mongodb://${config.HOST}:${config.PORT}/${config.DB_NAME}`)
+  .then((data) => console.log('Connection success'))
+  .catch((err) => console.log('Connection error', err));
+
+// экспорт моделей
+module.exports.User = require('/user');
