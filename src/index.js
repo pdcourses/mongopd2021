8683615af8ef50f17d2072c615ef2c4f37ce9663
julietvt <freshitpt@gmail.com>
2022-01-30 11:55:13 +0200

    connection wascreated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello Mongo');
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+db.on('error', () => {
+  process.exit(1);
+});
+db.once('open', function () {
+  console.log('connected');
+});
+console.log(db);
+
+mongoose.connect('mongod://127.0.0.1:27017/testpd2021', {useUnifiedTopology = true}, function(err){
+    if(err){ console.log('mongo connection error');
+    } else{
+        console.log('mongo connection success');
+    }
+});
